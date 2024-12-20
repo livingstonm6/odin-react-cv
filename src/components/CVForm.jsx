67b3ddfb7759f23aecb174d72932a795965ef21d
livingstonm6 <livingstonm6@mymacewan.ca>
2024-12-19 22:57:21 -0700

    Created basic form and removed unnecessary components.

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -1,14 +1,50 @@
-import Info from "./Info.jsx";
-import Education from "./Education.jsx";
-import Experience from "./Experience.jsx";
-
-function CVForm(props) {
+function CVForm() {
     return(
-        <div className="cv-form">
-            <Info></Info>
-            <Education></Education>
-            <Experience></Experience>
-        </div>
+        <>
+            <form>
+                <h1>Resume Creator</h1>
+                <h2>Personal Information</h2>
+                <label htmlFor="first-name">First Name</label>
+                <input id="first-name" type="text"/>
+                <label htmlFor="last-name">Last Name</label>
+                <input id="last-name" type="text"/>
+                <label htmlFor="address">Address</label>
+                <input id="address" type="text"/>
+                <label htmlFor="email">Email Address</label>
+                <input id="email" type="email"/>
+                <label htmlFor="phone">Phone Number</label>
+                <input id="phone" type="tel"/>
+
+                <h2>Education</h2>
+                <label htmlFor="school">School</label>
+                <input id="school" type="text"/>
+                <label htmlFor="school-location">Location</label>
+                <input id="school-location" type="text"/>
+                <label htmlFor="degree">Degree</label>
+                <input id="degree" type="text"/>
+                <label htmlFor="major">Area of Study (Major)</label>
+                <input id="major" type="text"/>
+                <label htmlFor="school-start-date">Start Date</label>
+                <input id="school-start-date" type="datetime-local"/>
+                <label htmlFor="school-end-date">End Date</label>
+                <input id="school-end-date" type="datetime-local"/>
+
+                <h2>Work Experience</h2>
+                <label htmlFor="job-title">Job Title</label>
+                <input id="job-title" type="text"/>
+                <label htmlFor="company">Company</label>
+                <input id="company" type="text"/>
+                <label htmlFor="job-location">Location</label>
+                <input id="job-location" type="text"/>
+                <label htmlFor="job-start-date">Start Date</label>
+                <input id="job-start-date" type="datetime-local"/>
+                <label htmlFor="job-end-date">End Date:</label>
+                <input id="job-end-date" type="datetime-local"/>
+                <label htmlFor="job-description">Description</label>
+                <input id="job-description" type="text"/>
+
+            </form>
+        </>
     )
 }
 
--- src/components/Education.jsx
+++
@@ -1,9 +0,0 @@
-function Education() {
-    return (
-        <>
-            <h1>Education</h1>
-        </>
-    )
-}
-
-export default Education;
