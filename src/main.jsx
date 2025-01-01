dcbc590ff952b56f7c59727d496179d88580d1ec
livingstonm6 <livingstonm6@mymacewan.ca>
2024-12-31 19:04:51 -0700

    Removed App component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App.jsx'
 import './styles/index.css'
+import './styles/App.css'
+import CVForm from "./components/CVForm.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <CVForm></CVForm>
   </StrictMode>,
 )
